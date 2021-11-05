cdfa2b37836192e95fc01ca1e54432fd10c9f718
ATolokonnikov <ATolokonnikov@sportmaster.ru>
2021-11-05 15:33:24 +0300

    Уроки 5 раздела

--- MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/BookService.java
+++ MyBookShopApp/src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -11,24 +11,14 @@ import java.util.List;
 @Service
 public class BookService {
 
-    private JdbcTemplate jdbcTemplate;
+    private BookRepository bookRepository;
 
     @Autowired
-    public BookService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
     }
 
     public List<Book> getBookData() {
-        List<Book> books = jdbcTemplate.query("select a.last_name as author, b.title, b.priceOld, b.price from books b, authors a where a.id = b.author_id",
-                (ResultSet rs, int rownum) -> {
-                    Book book = new Book();
-//                    book.setId(rs.getInt("id"));
-                    book.setAuthor(rs.getString("author"));
-                    book.setTitle(rs.getString("title"));
-                    book.setPriceOld(rs.getString("priceOld"));
-                    book.setPrice(rs.getString("price"));
-                    return book;
-                });
-        return new ArrayList<>(books);
+        return bookRepository.findAll();
     }
 }
