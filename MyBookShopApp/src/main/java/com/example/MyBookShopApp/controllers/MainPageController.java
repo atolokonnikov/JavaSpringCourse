1f4f016555d3d60e73a351f2d1e1c7453eb325f0
ATolokonnikov <ATolokonnikov@sportmaster.ru>
2022-02-13 13:11:50 +0300

    6.5 Spring REST Pagination (урок)

--- MyBookShopApp/src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
+++ MyBookShopApp/src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -2,10 +2,13 @@ package com.example.MyBookShopApp.controllers;
 
 import com.example.MyBookShopApp.data.Book;
 import com.example.MyBookShopApp.data.BookService;
+import com.example.MyBookShopApp.data.RecommendedBooksPageDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -20,12 +23,18 @@ public class MainPageController {
     }
 
     @ModelAttribute("recommendedBooks")
-    public List<Book> recommendedBooks(){
-        return bookService.getBookData();
+    public List<Book> recommendedBooks() {
+        return bookService.getPageOfRecommendedBooks(0, 6).getContent();
     }
 
     @GetMapping("/")
     public String mainPage() {
         return "index";
+    }
+
+    @GetMapping("/books/recommended")
+    @ResponseBody
+    public RecommendedBooksPageDto getBooksPage(@RequestParam("offset") Integer offset, @RequestParam("limit") Integer limit) {
+        return new RecommendedBooksPageDto(bookService.getPageOfRecommendedBooks(offset, limit).getContent());
     }
 }
